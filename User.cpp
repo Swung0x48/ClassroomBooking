ae8721c83c3e5686942cafbeb15d5664dea6666f
敏 何 <1036873334@qq.com>
2020-06-24 19:23:43 +0800

    user.cpp

--- User.cpp
+++ User.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <iomanip>
 #include "User.h"
+using namespace std;
+
+User::User(string account, string password, int type) :
+	_account(account),
+	_password(password),
+	_type(type)
+{}
+
+istream& operator>> (istream& input, User& u) {
+    string account;
+    string password;
+    int type;
+
+    input >> quoted(account)
+        >> quoted(password)
+        >> type;
+
+    u = User(account, password, type);
+
+    return input;
+}
+ostream& operator<<(ostream& output, User& u)
+{
+    output << u._account
+        << " "
+        << u._password
+        << " "
+        << u._type
+        << endl;
+
+    return output;
+}
