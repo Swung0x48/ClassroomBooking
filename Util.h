219157089be34212e0e9dff911c57421bcc05079
Swung0x48 <swung0x48@outlook.com>
2020-06-24 18:19:26 +0800

    to_string()

--- Util.h
+++ Util.h
@@ -14,6 +14,22 @@ private:
     static default_random_engine randEngine;
 public:
     static int getRandom() { return randEngine(); }
+    static string to_string(int val)
+    {
+        char buf[20];
+        sprintf(buf, "%d", val);
+        return string(buf);
+    }
+    static string to_string(double val)
+    {
+        char buf[2000];
+        sprintf(buf, "%.2f", val);
+        return string(buf);
+    }
+    static string to_string(const char* val)
+    {
+        return string(val);
+    }
 };
 
 #endif //LIBRARYMANAGER_UTIL_H
