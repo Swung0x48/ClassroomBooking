2126bd1b5c5249a87a84f3bb6ca80543c1044b14
敏 何 <1036873334@qq.com>
2020-06-25 23:22:56 +0800

    item/book

--- Library.cpp
+++ Library.cpp
@@ -13,11 +13,11 @@ void Library::AddBook(Book &book, int quantity)
     _stock[book] += quantity;
 }
 int Library::Statistics(int type) {
-    Book b;
     int sum=0;
     for (auto item : _stock) {
-        if (b.GetType() == type) {
-            sum += _stock[b];
+        Book book = item.first;
+        if (book.GetType() == type) {
+            sum += _stock[book];
         }
     }
     return sum;
@@ -47,22 +47,18 @@ void Library::Lend(int number, int quantity) {
     }
 }
 void Library::Return(int number, int quantity) {
-    Book b;
     for (auto item : _stock) {
-        if (b.GetNumber() == number) {
-            _stock[b] += quantity;
+        Book book = item.first;
+        if (book.GetNumber() == number) {
+            _stock[book] += quantity;
         }
         //TODO: get returntime
     }
 }
 Book Library::Search(int number)
 {
-//    Book b;
     for (auto item : _stock)
     {
-//        if (b.GetNumber() == number) {
-//            return b;
-//        }
         if (item.first.GetNumber() == number)
         {
             return item.first;
