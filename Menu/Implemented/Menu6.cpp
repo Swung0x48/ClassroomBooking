5a42d1e94f837232777bcc0ae513fb08d551b1f0
敏 何 <1036873334@qq.com>
2020-06-28 23:34:06 +0800

    Menu6

---
+++ Menu/Implemented/Menu6.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include "Menu6.h"
+#include "../../Books/Book.h"
+#include "../../Vars.h"
+#include "../../Books/SocialBook.h"
+#include "../../Books/ScienceBook.h"
+#include "../../Books/ForeignBook.h"
+
+using namespace std;
+
+void Menu6::Run() {
+	cout << "——————查询借阅信息——————" << endl;
+	cout << endl;
+	cout << "请输入需要查询的借阅人姓名：" << endl;
+	fflush(stdin);
+	string name; getline(cin, name);
+
+	try {
+		Vars::library.QueryLog(name);
+		int size,i=0;
+		cout << Vars::library.QueryLog(name)[i].GetBorrower() << "的历史借阅信息如下：" << endl;
+		cout << endl;
+		size = Vars::library.QueryLog(name).size();
+		while (i < size) {
+			cout << "图书编号："
+				<< Vars::library.QueryLog(name)[i].GetBookNumber()
+			    << "数量："
+				<< Vars::library.QueryLog(name)[i].GetQuantity();
+			i++;
+		}
+	}
+	catch (const char* c) {
+			cout << c << endl;
+	}
+}
---
+++ Menu/Implemented/Menu6.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#ifndef MENU6_H
+#define MENU6_H
+
+#include "../IMenu.h"
+#include <iostream>
+using namespace std;
+
+class Menu6 : public IMenu
+{
+protected:
+    int _option = 6;
+public:
+    virtual int GetOption() { return _option; }
+    virtual void Run();
+};
+
+
+#endif //MENU6_H
