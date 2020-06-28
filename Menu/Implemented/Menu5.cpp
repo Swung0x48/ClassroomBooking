326d88934e49b4b8561b259607494d6cc59ed2b1
敏 何 <1036873334@qq.com>
2020-06-29 00:43:30 +0800

    form modifying

--- Menu/Implemented/Menu5.cpp
+++ Menu/Implemented/Menu5.cpp
@@ -11,10 +11,10 @@ using namespace std;
 void Menu5::Run() {
 	cout << "——————统计图书——————" << endl;
 	cout << endl;
-	cout << "                您可以选择以下类型：" << endl;
-	cout << "                1.自然科学类" << endl;
-	cout << "                2.人文社科类" << endl;
-	cout << "                3.外文类" << endl;
+	cout << "您可以选择以下类型：" << endl;
+	cout << "1.自然科学类" << endl;
+	cout << "2.人文社科类" << endl;
+	cout << "3.外文类" << endl;
 	cout << endl;
 	cout << "请输入需要统计的图书类型：";
 	int opt;
